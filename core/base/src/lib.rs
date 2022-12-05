3cac8beada7c969098d6872f57981b24ee20ec94
wujunhua <wujunhua@uino.com>
2022-12-05 09:57:01 +0800

    build: add workspace

---
+++ core/base/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+#[no_mangle]
+pub extern fn hello() {
+    println!("hello Rust DLL!");
+}
